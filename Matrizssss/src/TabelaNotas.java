535c0789999268740e25c5726ee39c1e54d2de9a
MarkuwM <silvaalderlindson@gmail.com>
2024-11-26 20:13:32 -0300

    é hoje

--- Matrizssss/src/TabelaNotas.java
+++ Matrizssss/src/TabelaNotas.java
@@ -3,20 +3,19 @@ import java.util.Scanner;
 public class TabelaNotas {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        int[][] notas = new int[3][4];
-        String aluno1 = "";
-
-        boolean condicao = true;
-        while (condicao = true) {
-            System.out.println("Digite o nome do aluno");
-            input.next()
-            if ()
-
-            }
-        }
-
+        String[][] notas = new String[5][5];
+        notas[0][0] = "Aluno";
+        notas[0][1] = "";
+        notas[0][2] = "";
+        notas[0][3] = "";
+        notas[0][4] = "";
     }
-    public static void repeticaoNota() {
-    return null;
+    public static void percorrerMatriz(String[][] matriz) {
+        for (int i = 0; i < matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                System.out.print(matriz[i][j] + "\t");
+            }
+            System.out.println();
+        }
     }
 }
