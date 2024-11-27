f403d1b78b2f5036b8de776dc469e1cfcb0fe3a7
MarkuwM <silvaalderlindson@gmail.com>
2024-11-27 19:59:34 -0300

    tabela

--- Matrizssss/src/TabelaNotas.java
+++ Matrizssss/src/TabelaNotas.java
@@ -1,4 +1,3 @@
-import java.awt.*;
 import java.util.Scanner;
 
 public class TabelaNotas {
@@ -12,9 +11,11 @@ public class TabelaNotas {
         notas[0][3] = "Etapa 3";
         notas[0][4] = "Média";
 
+        preencherNomes(notas);
+        preencherZeros(notas);
         percorrerMatriz(notas);
 
-        for (int i = 0; i < notas.length; i++) {
+        for (int i = 1; i < notas.length; i++) {
             for (int j = 0; j < notas.length; j++) {
                 if (j == 0) {
                     System.out.println("Digite o nome do Aluno");
@@ -34,6 +35,11 @@ public class TabelaNotas {
                 }
             }
         }
+        percorrerMatriz(notas);
+        calcularMedias(notas);
+        percorrerMatriz(notas);
+        printMaiorNota(notas);
+        printMenorNota(notas);
     }
     public static void percorrerMatriz(String[][] matriz) {
         for (int i = 0; i < matriz.length; i++) {
@@ -46,7 +52,7 @@ public class TabelaNotas {
     public static void calcularMedias(String[][] matriz) {
         for (int i = 1; i < matriz.length; i++) {
             int soma = 0;
-            for (int j = 1; j < matriz.length; j++) {
+            for (int j = 1; j < matriz.length - 1; j++) {
                 int valor = Integer.valueOf(matriz[i][j]);
                 soma += valor;
 
@@ -56,9 +62,58 @@ public class TabelaNotas {
     }
     public static void printMaiorNota(String[][] matriz) {
         int maiorNota = 1;
-        String Aluno = "";
-        String Etapa = "";
+        String aluno = "";
+        String etapa = "";
 
+        for (int i = 1; i < matriz.length - 1; i++) {
+            for (int j = 1; j < matriz.length - 1; j++) {
+                int nota = Integer.valueOf(matriz[i][j]);
+                if (nota >  maiorNota) {
+                    maiorNota = nota;
+                    etapa = "Etapa " + j;
+                    aluno = matriz[i][0];
+                }
+            }
+            System.out.println("--------------------------");
+            System.out.println("Maior Nota: " + maiorNota);
+            System.out.println("Aluno: " + aluno);
+            System.out.println(etapa);
+            System.out.println("--------------------------");
+        }
+    }
+    public static void printMenorNota(String[][] matriz) {
+        int menorNota = Integer.MAX_VALUE;
+        String aluno = "";
+        String etapa = "";
 
+        for (int i = 1; i < matriz.length - 1; i++) {
+            for (int j = 1; j < matriz.length - 1; j++) {
+                int nota = Integer.valueOf(matriz[i][j]);
+                if (nota < menorNota) {
+                    menorNota = nota;
+                    etapa = "Etapa " + j;
+                    aluno = matriz[i][0];
+                }
+            }
+        }
+        System.out.println("--------------------------");
+        System.out.println("Menor Nota: " + menorNota);
+        System.out.println("Aluno: " + aluno);
+        System.out.println(etapa);
+        System.out.println("--------------------------");
+    }
+    public static void preencherZeros(String[][] matriz) {
+        for (int i = 1; i < matriz.length; i++) {
+            for (int j = 1; j < matriz.length; j++) {
+                matriz[i][j] = String.valueOf(0);
+            }
+        }
+    }
+    public static void preencherNomes(String[][] matriz) {
+        for (int i = 1; i < matriz.length; i++) {
+            for (int j = 0; j < matriz.length; j++) {
+                matriz[i][0] = "nome";
+            }
+        }
     }
 }
